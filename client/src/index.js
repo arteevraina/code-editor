35c2799123f9b784ed5cafff0e64b9bda6f88741
falguniraina <rainadiv01@gmail.com>
2020-11-18 18:44:16 +0530

    for web languages

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom";
 import Web from "./web-based/web";
+import './web-based/index.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Web />, document.getElementById("web"));
+ReactDOM.render(
+    <React.StrictMode>
+      <Web />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
--- client/src/web-based/web.js
+++ client/src/web-based/web.js
@@ -1,8 +1,63 @@
-import React, { useState, useContext } from "react";
+import React , { useState, useEffect } from "react";
+import Editor from './Editor'
+import useLocalStorage from './hooks/useLocalStorage'
+
+function Web() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
 //import { GlobalProvider } from "./context/GlobalState";
 
-const Web = () => {
-  return <h1>hello falguni, yha se banana shuru kr</h1>;
-};
+  return ( 
+  <>
+  <div className="pane top-pane">
+    <Editor
+      language="xml"
+      displayName="HTML"
+      value={html}
+      onChange={setHtml}
+    />
+    <Editor
+      language="css"
+      displayName="CSS"
+      value={css}
+      onChange={setCss}
+    />
+    <Editor
+      language="javascript"
+      displayName="JS"
+      value={js}
+      onChange={setJs}
+    />
+  </div>
+  <div className="pane">
+    <iframe
+      srcDoc={srcDoc}
+      title="output"
+      sandbox="allow-scripts"
+      frameBorder="0"
+      width="100%"
+      height="100%"
+    />
+  </div>
+</>
+  )
+}
 
 export default Web;
