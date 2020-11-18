2d30bb3d4399e8866e938cb622bfcc8712289e6a
BIraj <birag.gpta@gmail.com>
2020-11-18 19:58:30 +0530

    fixed interface problem

--- client/public/interface.js
+++ client/public/interface.js
@@ -3,9 +3,11 @@ var selector = document.querySelector(".cat");
 function def() {
   document.querySelector("#root").style.display = "block";
   document.querySelector("#web").style.display = "none";
+  console.log("root");
 }
 
 function web() {
   document.querySelector("#root").style.display = "none";
   document.querySelector("#web").style.display = "block";
+  console.log("web");
 }
