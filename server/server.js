c80d912cb5dc08940f4d698abacd021c3b4c7a5c
BIraj <birag.gpta@gmail.com>
2021-03-03 01:29:43 +0530

    changes for making live

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+const publicPath = path.join(__dirname, "..", "client", "build");
+const port = process.env.PORT || 8000;
 // IMPORTING ALL THE FILES
 const code = require("./api/code");
 app.use("/api/code", code);
 
-const port = process.env.PORT || 8000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(publicPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
